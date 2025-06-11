9a4d7c12f8ee1e45442af665e8992ef094f97352
juandy7 <juandiroca@hotmail.com>
2025-06-11 13:53:30 -0500

    Se implementò el ingres para Api-gateway

--- proxy-client/src/main/java/com/selimhorri/app/ProxyClientApplication.java
+++ proxy-client/src/main/java/com/selimhorri/app/ProxyClientApplication.java
@@ -4,18 +4,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
 public class ProxyClientApplication {
-	
-	public static void main(String[] args) {
-		SpringApplication.run(ProxyClientApplication.class, args);
-	}
-	
-	
-	
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxyClientApplication.class, args);
+    }
+
+    @RestController
+    public static class HealthController {
+        @GetMapping("/health")
+        public String health() {
+            return "success";
+        }
+    }
 }
 
 
